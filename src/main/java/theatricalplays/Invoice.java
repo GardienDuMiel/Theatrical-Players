328eaae5e63dacb05a6a7ea9b16f687e27f47526
Benoît <benoit.antunes.ba@gmail.com>
2023-10-29 20:39:16 +0100

    ajout toHTML() et toText() Invoice.java

--- src/main/java/theatricalplays/Invoice.java
+++ src/main/java/theatricalplays/Invoice.java
@@ -1,21 +1,29 @@
-// Package pour les représentations théâtrales
 package theatricalplays;
 
-// Importation de la liste
 import java.util.List;
+import java.util.Map;  // Ajout de l'importation nécessaire pour la classe Map
 
 // Classe représentant une facture
 public class Invoice {
 
-  // Nom du client
   public String customer;
-  
-  // Liste des représentations associées à la facture
   public List<Performance> performances;
 
   // Constructeur de la classe Invoice
   public Invoice(String customer, List<Performance> performances) {
     this.customer = customer;
     this.performances = performances;
+  }
+
+  // Méthode pour générer la facture au format HTML
+  public String toHTML(Map<String, Play> plays) {
+    HtmlStatementPrinter htmlStatementPrinter = new HtmlStatementPrinter();
+    return htmlStatementPrinter.generateHtml(this, plays);
+  }
+
+  // Méthode pour générer la facture au format texte
+  public String toText(Map<String, Play> plays) {
+    StatementPrinter statementPrinter = new StatementPrinter();
+    return statementPrinter.print(this, plays);
   }
 }

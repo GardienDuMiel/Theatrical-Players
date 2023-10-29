7d2d67cfe16a05e2f9f03c2e44c09ae28d0e016d
Benoît <benoit.antunes.ba@gmail.com>
2023-10-29 20:01:06 +0100

    ajout commentaire invoice.java

--- src/main/java/theatricalplays/Invoice.java
+++ src/main/java/theatricalplays/Invoice.java
@@ -1,12 +1,19 @@
+// Package pour les représentations théâtrales
 package theatricalplays;
 
+// Importation de la liste
 import java.util.List;
 
+// Classe représentant une facture
 public class Invoice {
 
+  // Nom du client
   public String customer;
+  
+  // Liste des représentations associées à la facture
   public List<Performance> performances;
 
+  // Constructeur de la classe Invoice
   public Invoice(String customer, List<Performance> performances) {
     this.customer = customer;
     this.performances = performances;

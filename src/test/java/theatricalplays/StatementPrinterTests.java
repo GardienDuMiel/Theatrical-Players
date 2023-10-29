a22d9879edd34ced11f57fabc7e244f4a73443bd
Benoît <benoit.antunes.ba@gmail.com>
2023-10-29 20:56:05 +0100

    ajout du test html

--- src/test/java/theatricalplays/StatementPrinterTests.java
+++ src/test/java/theatricalplays/StatementPrinterTests.java
@@ -60,4 +60,24 @@ public class StatementPrinterTests {
             statementPrinter.print(invoice, plays);
         });
     }
+
+     // Test d'acceptation pour le format HTML
+     @Test
+     void htmlStatement() {
+         Map<String, Play> plays = Map.of(
+                 "hamlet",  new Play("Hamlet", "tragedy"),
+                 "as-like", new Play("As You Like It", "comedy"),
+                 "othello", new Play("Othello", "tragedy"));
+ 
+         Invoice invoice = new Invoice("BigCo", List.of(
+                 new Performance("hamlet", 55),
+                 new Performance("as-like", 35),
+                 new Performance("othello", 40)));
+ 
+         // Génération de la facture au format HTML
+         String htmlResult = invoice.toHTML(plays);
+ 
+         // Approbation du résultat HTML
+         verify(htmlResult);
+     }
 }

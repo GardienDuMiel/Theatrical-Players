a1a3d39e8d186a3bbae38dcf51ce261d6baee950
Benoît <benoit.antunes.ba@gmail.com>
2023-10-29 20:16:21 +0100

    création variable statiques Play.java

--- src/main/java/theatricalplays/Play.java
+++ src/main/java/theatricalplays/Play.java
@@ -1,8 +1,13 @@
-// Package pour les représentations théâtrales
 package theatricalplays;
 
 // Classe représentant une pièce de théâtre
 public class Play {
+
+  // Types de pièces de théâtre en tant que variables statiques
+  public static final String TRAGEDY = "tragedy";
+  public static final String COMEDY = "comedy";
+  public static final String HISTORY = "history";
+  public static final String PASTORAL = "pastoral";
 
   // Nom de la pièce de théâtre
   public String name;
@@ -10,8 +15,13 @@ public class Play {
   // Type de la pièce de théâtre
   public String type;
 
-  // Constructeur de la classe Play
+  // Constructeur de la classe Play avec contrôle sur le type de pièce
   public Play(String name, String type) {
+    // Vérification du type de pièce lors de la création de l'objet
+    if (!type.equals(TRAGEDY) && !type.equals(COMEDY) && !type.equals(HISTORY) && !type.equals(PASTORAL)) {
+      throw new IllegalArgumentException("Invalid play type");
+    }
+
     this.name = name;
     this.type = type;
   }

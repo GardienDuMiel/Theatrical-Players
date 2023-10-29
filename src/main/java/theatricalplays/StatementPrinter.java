9be1449e463fe8d6a951b0ebd9d11ca763d22fd9
Benoît <benoit.antunes.ba@gmail.com>
2023-10-29 20:20:04 +0100

    gestion sommes en flottant StatementPrinter.java

--- src/main/java/theatricalplays/StatementPrinter.java
+++ src/main/java/theatricalplays/StatementPrinter.java
@@ -6,48 +6,56 @@ import java.util.Map;
 
 public class StatementPrinter {
 
+  // Méthode pour imprimer l'état financier à partir d'une facture et d'une liste de pièces de théâtre
   public String print(Invoice invoice, Map<String, Play> plays) {
-    int totalAmount = 0;
+    // Utilisation de double pour les sommes
+    double totalAmount = 0;
     int volumeCredits = 0;
 
     // Utilisation de StringBuffer pour concaténer les résultats
     StringBuffer result = new StringBuffer(String.format("Statement for %s\n", invoice.customer));
 
+    // Formatage de la monnaie en dollars américains
     NumberFormat frmt = NumberFormat.getCurrencyInstance(Locale.US);
 
+    // Parcours des représentations dans la facture
     for (Performance perf : invoice.performances) {
       Play play = plays.get(perf.playID);
-      int thisAmount = 0;
+      double thisAmount = 0; // Utilisation de double pour le montant
 
+      // Calcul du montant en fonction du type de la pièce de théâtre
       switch (play.type) {
         case "tragedy":
-          thisAmount = 40000;
+          thisAmount = 400.0;
           if (perf.audience > 30) {
-            thisAmount += 1000 * (perf.audience - 30);
+            thisAmount += 10.0 * (perf.audience - 30);
           }
           break;
         case "comedy":
-          thisAmount = 30000;
+          thisAmount = 300.0;
           if (perf.audience > 20) {
-            thisAmount += 10000 + 500 * (perf.audience - 20);
+            thisAmount += 100.0 + 5.0 * (perf.audience - 20);
           }
-          thisAmount += 300 * perf.audience;
+          thisAmount += 3.0 * perf.audience;
           break;
         default:
           throw new Error("unknown type: ${play.type}");
       }
 
+      // Ajout des crédits de volume
       volumeCredits += Math.max(perf.audience - 30, 0);
+      // Ajout de crédits supplémentaires pour chaque tranche de dix spectateurs pour les comédies
       if ("comedy".equals(play.type)) volumeCredits += Math.floor(perf.audience / 5);
 
       // Utilisation de StringBuffer pour concaténer la ligne résultat
-      result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount / 100), perf.audience));
+      result.append(String.format("  %s: %s (%s seats)\n", play.name, frmt.format(thisAmount), perf.audience));
       totalAmount += thisAmount;
     }
 
-    result.append(String.format("Amount owed is %s\n", frmt.format(totalAmount / 100)));
+    // Utilisation de StringBuffer pour concaténer les lignes du résultat
+    result.append(String.format("Amount owed is %s\n", frmt.format(totalAmount)));
     result.append(String.format("You earned %s credits\n", volumeCredits));
-    
+
     // Conversion de StringBuffer en String
     return result.toString();
   }

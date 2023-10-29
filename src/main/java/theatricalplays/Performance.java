92748132af829c2ba443b4addecf308d53e81a5c
Benoît <benoit.antunes.ba@gmail.com>
2023-10-29 20:02:14 +0100

    ajout commentaires Performance.java

--- src/main/java/theatricalplays/Performance.java
+++ src/main/java/theatricalplays/Performance.java
@@ -1,10 +1,16 @@
+// Package pour les représentations théâtrales
 package theatricalplays;
 
+// Classe représentant une représentation théâtrale
 public class Performance {
 
+  // Identifiant de la pièce de théâtre
   public String playID;
+  
+  // Nombre de spectateurs présents à la représentation
   public int audience;
 
+  // Constructeur de la classe Performance
   public Performance(String playID, int audience) {
     this.playID = playID;
     this.audience = audience;

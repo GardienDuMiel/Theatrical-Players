5cd5fb0cf2fca28b833fce6d8581d39daa322afd
Benoît <benoit.antunes.ba@gmail.com>
2023-10-29 20:03:30 +0100

    ajout commentaires Play.java

--- src/main/java/theatricalplays/Play.java
+++ src/main/java/theatricalplays/Play.java
@@ -1,10 +1,16 @@
+// Package pour les représentations théâtrales
 package theatricalplays;
 
+// Classe représentant une pièce de théâtre
 public class Play {
 
+  // Nom de la pièce de théâtre
   public String name;
+  
+  // Type de la pièce de théâtre
   public String type;
 
+  // Constructeur de la classe Play
   public Play(String name, String type) {
     this.name = name;
     this.type = type;
